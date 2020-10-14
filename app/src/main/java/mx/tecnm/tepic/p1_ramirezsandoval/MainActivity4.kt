eed00b63b28b83f2761119ede58ecc24182e55cf
Marco Antonio Ramirez Sandoval <60723739+Asouny@users.noreply.github.com>
2020-10-14 10:15:35 -0600

    Primer Commit
    Primer commit

---
+++ app/src/main/java/mx/tecnm/tepic/p1_ramirezsandoval/MainActivity4.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.p1_ramirezsandoval
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
